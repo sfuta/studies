045b611288f4923730a9f4fb5699e92d9c4ced19
sfutagami <sbuta23dev@yahoo.co.jp>
2021-11-18 06:29:48 +0900

    02: loop処理および入力チェック処理実装

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
 
 
@@ -8,15 +9,34 @@ fn main() {
     // Show message
     println!("Guess the number!");
 
-    // Create guess number
+    // Create target number
     let secret_num = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_num);
+    // println!("The secret number is: {}", secret_num);
 
-    // Input...
-    println!("Please input your guess!");
-    let mut guess = String::new();  // mut is mutable
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    loop {
+        // Input...
+        println!("Please input your guess number(1-101)!");
+        let mut guess = String::new();  // mut is mutable
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
+        // Check validate
+        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num
+            , Err(_) => {
+                continue;
+            }
+        };
+        
+        println!("You guessed: {}", guess);
 
-    println!("You guessed: {}", guess);
-
+        // Input number compare to target number
+        match guess.cmp(&secret_num) {
+            Ordering::Less => println!("Too small!")
+            , Ordering::Greater => println!("Too large!")
+            , Ordering::Equal => {
+                println!("Match!");
+                break;
+            }
+        }
+    }
 }
