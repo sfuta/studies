d21d72425aaf2184f9ac20e60c857bce5733f216
sfutagami <sbuta23dev@yahoo.co.jp>
2021-11-18 05:58:03 +0900

    02: secret number作成処理実装

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 
 fn main() {
     // Show message
     println!("Guess the number!");
-    println!("Please input your guess!");
+
+    // Create guess number
+    let secret_num = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_num);
 
     // Input...
+    println!("Please input your guess!");
     let mut guess = String::new();  // mut is mutable
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
