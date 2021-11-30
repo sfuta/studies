4d93b551ba66a3972a5bf7e98ade8ccc2e5acb49
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-01 07:16:45 +0900

    07: module学習01

--- rust/projects/07.communicator/src/lib.rs
+++ rust/projects/07.communicator/src/lib.rs
@@ -1,3 +1,26 @@
+// compilerはsrc/lib.rsのみ確認を行う
+mod network;
+// network module separated file to network.rs
+// mod network {
+//     // if call, network::connect()
+//     fn connect() {
+//     }
+
+//     mod server {
+//         // if call, network::server::connect()
+//         fn connect() {
+//         }
+//     }
+// }
+
+mod client;
+// client module separated file
+// mod client {
+//     // if call, client::connect()
+//     fn connect() {
+//     }
+// }
+
 #[cfg(test)]
 mod tests {
     #[test]
