a27cd2ba122d0644eda37a5c52359a76e57ad2f1
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-04 10:56:06 +0900

    08: collection学習: String操作

--- rust/projects/08.collection/src/main.rs
+++ rust/projects/08.collection/src/main.rs
@@ -26,9 +26,47 @@ fn _vec() {
     for row in &rows {
         print!("{:?}\t", row);
     }
+    println!("");
+}
+
+fn _str() {
+    let mut s = "aaaa".to_string(); // = String::from("aaaa")
+    s.push_str(" bbbb");
+    let t = " cccc";
+    s.push_str(t);  // tの所有権は無くならない
+    println!("t={}", t);
+    s.push('.');
+    println!("s={}", s);
+
+    // let s1 = String::from("Hello, ");
+    // let s2 = String::from("world.");
+    // let s3 = s1 + &s2; // s1 is move
+    // println!("{}", s1);
+
+    let s1 = String::from("2021");
+    let s2 = String::from("12");
+    let s3 = String::from("31");
+    let _date = format!("{}-{}-{}", s1, s2, s3);
+    println!("date={}", _date);
+
+    let l = String::from("アイウエオ.");
+    // let c = l[0]; NG: Rust is not support for index use access.
+    let m = &l[0..6];
+    println!("m={}", m);
+
+    for c in l.chars() {
+        print!("{} ", c);
+    }
+    print!("\n");
+
+    for b in l.bytes() {
+        print!("{} ", b);
+    }
+    print!("\n");
 
 }
 
 fn main() {
     _vec();
+    _str();
 }
