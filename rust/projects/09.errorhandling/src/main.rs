feb3818470a25f955e9d1923e154c03c15a00857
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-08 06:57:48 +0900

    09: error handling学習: 検査のために独自の型を作る

--- rust/projects/09.errorhandling/src/main.rs
+++ rust/projects/09.errorhandling/src/main.rs
@@ -83,3 +83,22 @@ fn main() {
     // _use_expect();
     _error_transfer();
 }
+
+/**
+ * The follow is example for inspect class
+ */
+pub struct Guess {
+    value: u32,
+}
+impl Guess {
+    pub fn new(value: u32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}", value);
+        }
+        Guess { value }
+    }
+
+    pub fn value(&self) -> u32 {
+        return self.value;
+    }
+}
