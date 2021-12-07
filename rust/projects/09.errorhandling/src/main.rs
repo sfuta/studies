9f1c65c423a9ace297f0c63ef64f035c16762a05
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-08 06:38:41 +0900

    09: error handling学習: エラー移譲

--- rust/projects/09.errorhandling/src/main.rs
+++ rust/projects/09.errorhandling/src/main.rs
@@ -1,3 +1,7 @@
+use std::io;
+use std::io::Read;
+use std::fs::File;
+
 fn _panic() {
     panic!("Call panic!!");
 }
@@ -28,7 +32,6 @@ fn _errors_match() {
     };
 }
 
-use std::fs::File;
 
 fn _use_unwrap () {
     let _f = File::open("hello.txt").unwrap();
@@ -38,11 +41,45 @@ fn _use_expect() {
     let _f = File::open("hello.txt").expect("Failed open file. [hello.txt]");
 }
 
+fn _error_transfer() {
+    fn read_file_h_txt() -> Result<String, io::Error> {
+        let f = File::open("h.txt");
+        let mut f = match f {
+            Ok(file) => file, Err(e) => return Err(e),
+        };
+        let mut s = String::new();
+        match f.read_to_string(&mut s) {
+            Ok(_) => Ok(s), Err(e) => Err(e)
+        }
+    }
+
+    fn read_file_a_txt() -> Result<String, io::Error> {
+        let mut f = File::open("a.txt")?;
+        let mut s = String::new();
+        f.read_to_string(&mut s)?;
+        Ok(s)
+    }
+
+    fn read_file_b_txt() -> Result<String, io::Error> {
+        let mut s = String::new();
+        File::open("b.txt")?.read_to_string(&mut s)?;
+        Ok(s)
+    }
+
+    let _f = read_file_b_txt().expect("Failed read_file_b_txt");
+    let _f = read_file_a_txt().expect("Failed read_file_a_txt");
+    let _f = read_file_h_txt().unwrap();
+
+    // NG: '?' is available, when function returns 'Result'
+    // let _f = File::open("c.txt")?;
+}
+
 fn main() {
     // _out_of_bound();
     // _panic();
     // _use_result();
     // _errors_match();
     // _use_unwrap();
-    _use_expect();
+    // _use_expect();
+    _error_transfer();
 }
