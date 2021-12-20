453e9ad7de18992e16ca38b8438dae5fea760071
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-21 06:34:47 +0900

    12: make command: Add file io

--- rust/projects/12.minigrep/src/main.rs
+++ rust/projects/12.minigrep/src/main.rs
@@ -1,6 +1,20 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}\nin file {}", query, filename);
+
+    let mut contents = String::new();
+    let mut f = File::open(filename).expect("file not found");
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reaing the file");
+
+    println!("With text: \n{}", contents);
+
 }
