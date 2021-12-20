19707f0f337a60e2e54acc4b05d5b3a953c00ad0
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-21 07:02:10 +0900

    12: make command: Args parse separated

--- rust/projects/12.minigrep/src/main.rs
+++ rust/projects/12.minigrep/src/main.rs
@@ -5,16 +5,30 @@ use std::io::prelude::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}\nin file {}", query, filename);
+    println!("Searching for {}\nin file {}", config.query, config.filename);
 
     let mut contents = String::new();
-    let mut f = File::open(filename).expect("file not found");
+    let mut f = File::open(config.filename).expect("file not found");
     f.read_to_string(&mut contents)
         .expect("something went wrong reaing the file");
 
     println!("With text: \n{}", contents);
 
+}
+
+struct Config { query: String, filename: String }
+
+/**
+ * args parse 
+ * 
+ * @param &[String] args
+ * @return Config
+ */
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
