3ad3bed5439b03409ea3a74cd3ad41a5e1fefa1d
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-22 13:59:56 +0900

    12: make command: Separated logic

--- rust/projects/12.minigrep/src/main.rs
+++ rust/projects/12.minigrep/src/main.rs
@@ -1,7 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,26 +15,9 @@ fn main() {
 
     println!("Searching for {}\nin file {}", config.query, config.filename);
 
-    let mut contents = String::new();
-    let mut f = File::open(config.filename).expect("file not found");
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reaing the file");
-
-    println!("With text: \n{}", contents);
-
-}
-
-struct Config { query: String, filename: String }
-
-impl Config {
-
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
+
 }
