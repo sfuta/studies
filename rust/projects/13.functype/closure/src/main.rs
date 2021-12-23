622edee51652a6a80ffa0f55b6dda228dc77902c
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-24 06:58:08 +0900

    13: closure: Add closure sample

--- rust/projects/13.functype/closure/src/main.rs
+++ rust/projects/13.functype/closure/src/main.rs
@@ -1,23 +1,42 @@
 use std::thread;
 use std::time::Duration;
 
-fn simulated_expensive_calc(intensity: u32) -> u32 {
-    println!("Calculationing slowly...");
-    thread::sleep(Duration::from_secs(1));
-    intensity
+struct Cacher<T: Fn(u32) -> u32> {
+    calc : T, value: Option<u32>
+}
+impl<T> Cacher<T> where T: Fn(u32) -> u32 {
+    fn new(calc: T) -> Cacher<T> {
+        Cacher { calc, value: None }
+    }
+
+    fn value(&mut self, arg: u32) -> u32 {
+        match self.value {
+            Some(v) => v,
+            None => {
+                let v = (self.calc)(arg);
+                self.value = Some(v);
+                v
+            }
+        }
+    }
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num: u32| -> u32 {
+        println!("Calculationing slowly...");
+        thread::sleep(Duration::from_secs(1));
+        num
+    });
 
     if intensity < 25 {
         println!(
             "Today, do {} pushups!", 
-            simulated_expensive_calc(intensity)
+            expensive_result.value(intensity)
         );
 
         println!(
             "Next, do {} situps!", 
-            simulated_expensive_calc(intensity)
+            expensive_result.value(intensity)
         );
 
     } else {
@@ -26,7 +45,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!", 
-                simulated_expensive_calc(intensity)
+                expensive_result.value(intensity)
             );
         }
     }
@@ -42,6 +61,20 @@ fn _use_generate_workout() {
     )
 
 }
+
+fn _with_external_variable() {
+    let x = 4;
+    let equal_to_x = |z:u32| z == x;
+    let y = 4;
+    assert!(equal_to_x(y));
+
+    let x = vec![1, 2, 3];
+    let equal_to_x = move |z:Vec<u32>| { z == x };
+    // println!("can't use x here: {:?}, because x is moved", x);
+    let y = vec![1, 2, 3];
+    assert!(equal_to_x(y));
+}
 fn main() {
     _use_generate_workout();
+    _with_external_variable();
 }
