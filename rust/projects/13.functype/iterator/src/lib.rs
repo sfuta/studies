e6f8631906421b45643b4da4eb5032b43e196bb1
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-25 09:06:55 +0900

    13: iterator: consuming adaptors sample

--- rust/projects/13.functype/iterator/src/lib.rs
+++ rust/projects/13.functype/iterator/src/lib.rs
@@ -2,6 +2,15 @@
 mod my_tests {
 
     #[test]
+    fn iterator_sum() {
+        let v1 = vec![1, 2, 3];
+        let v1_iter = v1.iter();
+        let total: i32 = v1_iter.sum();
+
+        assert_eq!(total, 6);
+    }
+
+    #[test]
     fn iterator_demonstration() {
         let v1 = vec![1, 2, 3];
         let mut v1_iter = v1.iter();
