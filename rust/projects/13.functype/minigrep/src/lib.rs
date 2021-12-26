c3fb7d6f34dc7c6823d4b8ef52d6d98dd48eafdd
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-27 07:11:09 +0900

    13: minigrep feat: new() method feat

--- rust/projects/13.functype/minigrep/src/lib.rs
+++ rust/projects/13.functype/minigrep/src/lib.rs
@@ -8,12 +8,19 @@ pub struct Config { pub query: String, pub filename: String, pub case_sensitive:
 
 impl Config {
 
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg, 
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg, 
+            None => return Err("Didn't get a filename string"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
         Ok(Config { query, filename, case_sensitive })
