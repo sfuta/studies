1d7032bd801931229d4610d7ce9050eec4802864
sfutagami <sbuta23dev@yahoo.co.jp>
2021-12-28 06:38:45 +0900

    14: cargo,crates.io: Cargo doc example

--- rust/projects/14.cargo/sandbox/src/lib.rs
+++ rust/projects/14.cargo/sandbox/src/lib.rs
@@ -1,8 +1,25 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+/// 与えられた数値に1を足す
+/// Adds one to the number given
+/// 
+/// # Examples
+/// 
+/// ```rust
+/// let file = 5;
+/// 
+/// assert_eq!(6, sandbox::add_one(5));
+/// ```
+/// 
+/// # Panics
+/// 
+/// `Panic conditioins`
+/// 
+/// # Errors
+/// 
+/// `Error conditioins`
+/// 
+/// # Safety
+/// 
+/// `Unsafe detail`
+pub fn add_one(x: i32) -> i32 {
+    x + 1
 }
