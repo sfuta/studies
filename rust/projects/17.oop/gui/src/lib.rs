5bb69cc2dbbdf0a5c0b13fd98888acf417cafde3
sfutagami <sbuta23dev@yahoo.co.jp>
2022-01-07 07:18:50 +0900

    17: oop: extend

---
+++ rust/projects/17.oop/gui/src/lib.rs
@@ -0,0 +1,38 @@
+pub trait Draw {
+    fn draw(&self);
+}
+
+pub struct Screen {
+    pub components: Vec<Box<dyn Draw>>,
+}
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // TODO implement button draw
+        println!("Button draw: {:?}", self);
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
