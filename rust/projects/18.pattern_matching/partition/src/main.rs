556d6a01f7003d9d726017938b6af0d8a4a16081
sfutagami <sbuta23dev@yahoo.co.jp>
2022-01-12 06:36:48 +0900

    18: patturn matching: struct partition

--- rust/projects/18.pattern_matching/partition/src/main.rs
+++ rust/projects/18.pattern_matching/partition/src/main.rs
@@ -1,3 +1,21 @@
+struct Point {
+    x: i32, y: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let p1 = Point { x: 0, y: 7 };
+    let p2 = Point { x: 0, y: 7 };
+    let p3 = Point { x: 0, y: 7 };
+
+    let Point { x: a, y: b } = p1;
+    println!("a = {}, b = {}", a, b);
+
+    let Point { x, y } = p2;
+    println!("x = {}, y = {}", x, y);
+
+    match p3 {
+        Point { x, y: 0 } => println!("On the x axis at {}", x),
+        Point { x: 0, y } => println!("On the y axis at {}", y),
+        Point { x, y } => println!("On neither axis: ({}, {})", x, y),
+    }
 }
