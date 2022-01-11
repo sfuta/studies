b56ad3bb4833b5c52311a6883553ae296d289e82
sfutagami <sbuta23dev@yahoo.co.jp>
2022-01-12 07:06:30 +0900

    18: patturn matching: ref, tuple partition

--- rust/projects/18.pattern_matching/partition/src/main.rs
+++ rust/projects/18.pattern_matching/partition/src/main.rs
@@ -2,7 +2,7 @@ struct Point {
     x: i32, y: i32,
 }
 
-fn main() {
+fn _struct() {
     let p1 = Point { x: 0, y: 7 };
     let p2 = Point { x: 0, y: 7 };
     let p3 = Point { x: 0, y: 7 };
@@ -18,4 +18,27 @@ fn main() {
         Point { x: 0, y } => println!("On the y axis at {}", y),
         Point { x, y } => println!("On neither axis: ({}, {})", x, y),
     }
+
+}
+
+fn _ref() {
+    let points = vec![
+        Point { x: 0, y: 0 },
+        Point { x: 1, y: 5 },
+        Point { x: 10, y: -3 },
+    ];
+
+    let sum_of_squares: i32 = points.iter().map(|&Point { x, y }| x * x + y * y).sum();
+    println!("sum of points square {}", sum_of_squares);
+}
+
+fn _struct_taple() {
+    let ((feet, inches), Point {x, y}) = ((3, 10), Point { x: 3, y: -10 });
+    println!("feet = {}, inches = {}, x = {}, y = {}", feet, inches, x, y);
+}
+
+fn main() {
+    _struct();
+    _ref();
+    _struct_taple();
 }
