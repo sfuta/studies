134474864707a1a8b86452d61813550479e2acc0
sfutagami <sbuta23dev@yahoo.co.jp>
2022-01-21 06:42:08 +0900

    19: highed feature: lifetime: subtyping

---
+++ rust/projects/19.highed_feature/lifetime/src/lib.rs
@@ -0,0 +1,22 @@
+struct Context<'s>(&'s str);
+
+struct Parser<'c, 's: 'c> {
+    context: &'c Context<'s>,
+}
+impl<'c, 's> Parser<'c, 's> {
+    fn parse(&self) -> Result<(), &'s str> {
+        Err(&self.context.0[1..])
+    }
+}
+
+fn parse_context(context: Context) -> Result<(), &str> {
+    Parser { context: &context }.parse()
+}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
