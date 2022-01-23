5a07b7813d1c3dcbab280cbb3b4d3616985c9eb7
sfutagami <sbuta23dev@yahoo.co.jp>
2022-01-24 07:33:54 +0900

    19: highed feature: trait: super trait

--- rust/projects/19.highed_feature/high_trait/src/main.rs
+++ rust/projects/19.highed_feature/high_trait/src/main.rs
@@ -77,8 +77,38 @@ fn _full_path() {
 
 }
 
+use std::fmt;
+fn _outline_print() {
+
+    trait OutlinePrint: fmt::Display {
+        fn outline_print(&self) {
+            let output = self.to_string();
+            let len = output.len();
+
+            println!("{}", "*".repeat(len + 4));
+            println!("*{}*", " ".repeat(len + 2));
+            println!("* {} *", output);
+            println!("*{}*", " ".repeat(len + 2));
+            println!("{}", "*".repeat(len + 4));
+        }
+    }
+
+    struct Point { x: u32, y: u32, }
+
+    impl fmt::Display for Point {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "({}, {})", self.x, self.y)
+        }
+    }
+    impl OutlinePrint for Point {}
+
+    let p = Point { x: 4, y: 5 };
+    p.outline_print();
+}
+
 fn main() {
     println!("{:?}", Point { x: 1, y: 0} + Point { x: 2, y: 3});
     _same_name_methods();
     _full_path();
+    _outline_print();
 }
