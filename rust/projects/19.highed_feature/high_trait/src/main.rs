509fc45eb6ce15197c90279c915207c4407243ea
sfutagami <sbuta23dev@yahoo.co.jp>
2022-01-24 07:20:03 +0900

    19: highed feature: trait: full path note

--- rust/projects/19.highed_feature/high_trait/src/main.rs
+++ rust/projects/19.highed_feature/high_trait/src/main.rs
@@ -15,6 +15,70 @@ impl Add for Point {
     }
 }
 
+fn _same_name_methods() {
+
+    trait Pilot {
+        fn fly(&self);
+    }
+
+    trait Wizard {
+        fn fly(&self);
+    }
+
+    struct Human;
+
+    impl Pilot for Human {
+        fn fly(&self) {
+            println!("This is your captain speaking.");
+        }
+    }
+
+    impl Wizard for Human {
+        fn fly(&self) {
+            println!("Up!");
+        }
+    }
+
+    impl Human {
+        fn fly(&self) {
+            println!("*waving arms furiously*");
+        }
+    }
+
+    let person = Human;
+    Pilot::fly(&person);
+    Wizard::fly(&person);
+    person.fly();
+
+}
+
+fn _full_path() {
+
+    trait Animal {
+        fn baby_name() -> String;
+    }
+
+    struct Dog;
+
+    impl Dog {
+        fn baby_name() -> String {
+            String::from("Spot")
+        }
+    }
+
+    impl Animal for Dog {
+        fn baby_name() -> String {
+            String::from("puppy")
+        }
+    }
+
+    println!("A baby dog is called a {}", Dog::baby_name());
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
+
+}
+
 fn main() {
     println!("{:?}", Point { x: 1, y: 0} + Point { x: 2, y: 3});
+    _same_name_methods();
+    _full_path();
 }
