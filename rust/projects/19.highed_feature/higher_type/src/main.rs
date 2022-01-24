f8083d093f2707de59226d1776b00a38f6e9ddc2
sfutagami <sbuta23dev@yahoo.co.jp>
2022-01-25 07:29:52 +0900

    19: highed feature: type: never, dynamic sized

--- rust/projects/19.highed_feature/higher_type/src/main.rs
+++ rust/projects/19.highed_feature/higher_type/src/main.rs
@@ -16,6 +16,34 @@ fn _alias() {
     returns_long_type()();
 }
 
+fn _never() {
+    // TODO 実装方法が不明
+    // never typeはcontinue, println!(), panic!等で使用
+    // fn _return_empty(s: String) -> ! {
+    // }
+
+    // _return_empty(String::from("56"));
+}
+
+fn _dynamic_sized() {
+    // &str等のスライス型が動的型付け
+
+    // 動的な型付けとなる
+    fn _dyn_sized<T>(_t: T) {
+        // TODO
+    }
+    // -> ※下記のように扱われる
+    fn _express_dyn_sized<T: Sized>(_t: T) {
+        // TODO
+    }
+
+    fn _loose_dyn_sized<T: ?Sized>(_t: &T) {
+        // TODO
+    }
+}
+
 fn main() {
     _alias();
+    _never();
+    _dynamic_sized();
 }
