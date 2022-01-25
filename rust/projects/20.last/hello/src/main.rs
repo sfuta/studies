d6ff8a823ef661da31f6d55f6da74fccd3a076ac
sfutagami <sbuta23dev@yahoo.co.jp>
2022-01-26 07:27:42 +0900

    20: last: create simple web server: responsed html

--- rust/projects/20.last/hello/src/main.rs
+++ rust/projects/20.last/hello/src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 
@@ -16,7 +18,12 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let mut file = File::open("resource/hello.html").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
 }
