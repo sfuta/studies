1c44f5ae8272ae3ce8902a151c5344528452f7dc
sfutagami <sbuta23dev@yahoo.co.jp>
2022-01-28 06:53:30 +0900

    20: last: create simple web server: feat: multi thread
    - thread change to sharing owhership

--- rust/projects/20.last/hello/src/lib.rs
+++ rust/projects/20.last/hello/src/lib.rs
@@ -1,5 +1,7 @@
 use std::thread;
 use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
@@ -21,10 +23,12 @@ impl ThreadPool {
 
         let (sender, receiver) = mpsc::channel();
 
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id, receiver));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool {
@@ -42,7 +46,7 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: mpsc::Receiver<Job>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(|| {
             receiver;
         });
